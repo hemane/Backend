5603383d7cd6f69c2e98d05e622d8bce10fb9f01
Hendrik Heinle <hendrik.heile@heinleit.de>
2019-07-01 05:20:08 +0200

    Day-Route korrigiert

--- HeMaNe.Web/Controllers/DayController.cs
+++ HeMaNe.Web/Controllers/DayController.cs
@@ -11,7 +11,7 @@ using Microsoft.CodeAnalysis.Operations;
 
 namespace HeMaNe.Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class DayController : ControllerBase
     {
